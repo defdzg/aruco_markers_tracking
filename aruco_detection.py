b6844bfa2ada89af07ed1c08d96ce6119662d1db
Daniel Fernandez <xsteguin@gmail.com>
2022-10-28 11:12:02 -0500

    Current script allows to track inidvidual markers and show it's coordinates and it's rotation. It has been tested using 3 markers, and distance is drawn form a reference marker. Implementation in Python isn't useful to satisfy all the requirements and methods to achieve better performance and marker identification, we're switching to OpenCV and ArUco source library in C++ .

--- aruco_detection.py
+++ aruco_detection.py
@@ -10,6 +10,7 @@ import numpy as np
 import os
 import time
 from dotenv import load_dotenv
+from scipy.spatial.transform import Rotation as R
 
 # Existing ArUco dictionaries to be selected
 ARUCO_DICT = {
@@ -38,6 +39,92 @@ ARUCO_DICT = {
  
 }
 
+def inversePerspective(rvec, tvec):
+    
+    R, _ = cv2.Rodrigues(rvec)
+    R = np.matrix(R).T
+    invTvec = np.dot(R, np.matrix(-tvec))
+    invRvec, _ = cv2.Rodrigues(R)
+    return invRvec, invTvec
+
+def draw_frame_markers(corners, ids, rejected, frame, mtx, dist):
+    
+    flag = 0
+    
+    if len(corners) > 0:
+        
+        ids = ids.flatten()
+        for (markerCorner, markerID) in zip(corners, ids):
+            
+            # Get individual marker corners
+            rvec, tvec, _ = cv.aruco.estimatePoseSingleMarkers(markerCorner, 0.023, mtx, dist)
+            cv.drawFrameAxes(frame, mtx, dist, rvec, tvec, 0.01)
+            rotM = np.zeros(shape=(3,3))
+            cv.Rodrigues(rvec[0], rotM, jacobian = 0)
+            r = R.from_matrix(rotM)
+            angles = r.as_euler('zxy', degrees=True)
+            y = round(angles[0], 1)
+            r = round(angles[1], 1)
+            p = round(angles[2], 1)
+            corners = markerCorner.reshape((4, 2))
+            
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+            # Get each corner coordinates
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+            
+            # Draw each marker corners
+            cv.line(frame, topLeft, topRight, (0, 255, 0), 2)
+            cv.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+            cv.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+            
+            # Compute marker center coordinates
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            #cv.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+
+            # Display current marker center coordinates
+            cv.putText(frame, 
+            ('id: ' + str(markerID) + ', x:' + str(cX) + ', y:' + str(cY) + ', r:' + str(r) + ', p:' + str(p) + ', y:' + str(y)), 
+            (topLeft[0], topLeft[1] - 10), 
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 255, 0),
+            2)
+            
+            # Measuring distance in px and drawing lines from the reference marker to the moving marker
+            if (markerID == 2):
+                flag = 1
+                refcX = cX
+                refcY = cY
+            else:
+                if(flag == 1):
+                    cv.line(frame, (refcX,refcY), (cX, cY), (0, 255, 0), 2)
+                    if (markerID == 1):
+                        distanceX =  cX - refcX
+                        distanceY = refcY - cY
+                        cv.putText(frame, ('Dist1-2: (' + str(distanceX) + ',' + str(distanceY) + ') px'),
+                                   (1600, 70), 
+                                    cv.FONT_HERSHEY_SIMPLEX,
+                                    0.5,
+                                    (0, 255, 0),
+                                    2)
+                    if (markerID == 0):
+                        distanceX =  cX - refcX
+                        distanceY = refcY - cY
+                        cv.putText(frame, ('Dist0-2: (' + str(distanceX) + ',' + str(distanceY) + ') px'),
+                                   (1600, 100), 
+                                    cv.FONT_HERSHEY_SIMPLEX,
+                                    0.5,
+                                    (0, 255, 0),
+                                    2)
+
+    return frame 
+
 # Read existing camera calibration parameters
 def read_calibration(calibration_file):
     
@@ -60,7 +147,8 @@ def capture(model, src, width, height, fps, dict):
                           params=[
                               cv.CAP_PROP_FRAME_WIDTH, width,
                               cv.CAP_PROP_FRAME_HEIGHT,height,
-                              cv.CAP_PROP_FPS, fps])
+                              cv.CAP_PROP_FPS, fps,
+                              cv.CAP_PROP_AUTOFOCUS, 0 ])
     time.sleep(2.0)
     
     # Set ArUco dictionary and initalize parameters
@@ -74,16 +162,16 @@ def capture(model, src, width, height, fps, dict):
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         # Detect ArUco markers
         corners, ids, rejected = cv.aruco.detectMarkers(gray, arucoDict, parameters=arucoParams)
-        # Estimate detected markers pose
-        rvec, tvec, markerPointse = cv.aruco.estimatePoseSingleMarkers(corners, 0.023, mtx, dist);
+        # Estimate detected markers 
         
-        if len(corners) > 0:
-            for i in range(0, len(ids)):
-                
-                # Draw IDs, frames and corners for each detected marker
-                cv.drawFrameAxes(frame, mtx, dist, rvec[i, :, :], tvec[i, :, :], 0.01)
-                cv.aruco.drawDetectedMarkers(frame, corners, ids) 
+        rvec, tvec, _ = cv.aruco.estimatePoseSingleMarkers(corners, 0.04, mtx, dist);
+        cv.drawFrameAxes(frame, mtx, dist, rvec, tvec, 0.01)
+
+        #frame = draw_frame_markers(corners, ids, rejected, frame, mtx, dist)
         
+        newcameramtx, _ = cv.getOptimalNewCameraMatrix(mtx, dist, (width,height), 1, (width,height))
+        frame = cv.undistort(frame, mtx, dist, None, newcameramtx)
+
         # Display current fram with it's drawing
         cv.imshow('frame', frame)
         key = cv.waitKey(1) & 0xFF
